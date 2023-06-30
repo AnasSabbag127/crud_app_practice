3843d985f2986000e849b69c03924ed94a5ab1ed
Anas-Sabbag <anas.sabbag@remotestate.com>
2023-06-30 14:41:14 +0530

    connection to database done

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ mod model;
 mod repository;
 mod api;
 
-use actix_web :: {web ,App ,HttpRequest ,HttpServer ,Responder, HttpResponse };
-
+use actix_web::{web,App,HttpServer,Responder,HttpResponse};
+use actix_web::{http::header};
+use actix_web::middleware::Logger;
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions,Pool,Postgres};
 
 
 
@@ -16,10 +19,10 @@ use actix_web :: {web ,App ,HttpRequest ,HttpServer ,Responder, HttpResponse };
 // }
 
 
-async fn greet(req:HttpRequest) -> impl Responder{
-    let meet = req.match_info().get("meet").unwrap_or(" server");
-    format!("hello {}! ",meet)
+pub struct AppState{
+    db:Pool<Postgres>
 }
+
 
 async fn health_check()->impl Responder{
     HttpResponse::Ok()
@@ -28,11 +31,36 @@ async fn health_check()->impl Responder{
 #[actix_web :: main]
 
 async fn main() -> std::io::Result<()> {
-    // println!("Hello, world!");
+    
+    if std::env::var_os("RUST_LOG").is_none(){
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
 
-    HttpServer:: new(||{
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL")
+                .expect("database url must be set");
+    let pool =  match PgPoolOptions::new()
+                .max_connections(10)
+                .connect(&database_url)
+                .await
+                {
+                    Ok(pool) => {
+                        println!("connection to database is successful ! ");
+                        pool
+                    }
+                    Err(err) => {
+                        println!(" failed to  connect to database {:?} ",err);
+                        std::process::exit(1);
+                    }
+                };
+
+            println!("server connected successfully ");
+
+    HttpServer:: new(move ||{
         App::new()
-        .route("/",web::get().to(greet))
+        .app_data(web::Data::new(AppState{db:pool.clone()}))
         .route("/health_check",web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
